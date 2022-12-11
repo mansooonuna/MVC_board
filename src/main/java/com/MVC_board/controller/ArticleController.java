c771fb3795bb7870eb82c8cc303ac308d9ee4226
mansooonuna <parkdasom95@gmail.com>
2022-12-11 15:55:42 +0900

    showList 기능 추가

--- src/main/java/com/MVC_board/controller/ArticleController.java
+++ src/main/java/com/MVC_board/controller/ArticleController.java
@@ -10,26 +10,25 @@ import java.util.List;
 
 @Controller
 public class ArticleController {
-
     List<Article> articles = new ArrayList<>();
+    Article article;
     public int id = 1;
+
+    @RequestMapping("/usr/article/list")
+    @ResponseBody
+    public List<Article> showList() {
+        return articles;
+    }
 
     @RequestMapping("/usr/article/doAdd")
     @ResponseBody
     public Article doAdd(String title, String body) {
-
-        Article article = new Article(id, title, body);
+        article = new Article(id, title, body);
         articles.add(article);
         id++;
-
         return article;
     }
 
-    @RequestMapping("/usr/article/list")
-    @ResponseBody
-    public List<Article> list() {
-        return articles;
-    }
 
 
 }
