4c3071acd845e9ddbcd52145c11e1262cd512b80
mansooonuna <parkdasom95@gmail.com>
2022-12-13 16:43:14 +0900

    회원가입 정보 유효성 체크

--- src/main/java/com/MVC_board/controller/MemberController.java
+++ src/main/java/com/MVC_board/controller/MemberController.java
@@ -14,9 +14,30 @@ public class MemberController {
     @RequestMapping("/usr/member/join")
     @ResponseBody
     public String join(String loginId, String loginPw, String name, String nickname, String email, String cellphoneNo) {
+        if (loginId.isEmpty()) {
+            return "아이디를 입력해주세요.";
+        }
+        if (loginPw.isEmpty()) {
+            return  "비밀번호를 입력해주세요.";
+        }
+        if (name.isEmpty()) {
+            return "이름을 입력해주세요.";
+        }
+        if (nickname.isEmpty()) {
+            return  "닉네임을 입력해주세요.";
+        }
+        if (email.isEmpty()) {
+            return "이메일을 입력해주세요.";
+        }
+        if (cellphoneNo.isEmpty()) {
+            return  "전화번호를 입력해주세요.";
+        }
+
+
         if (loginId.equals(memberService.getLoginIdOfMember(loginId))) {
             return "해당 아이디(" + loginId + ")는 이미 존재하는 아이디 입니다.";
         }
+
         memberService.join(loginId, loginPw, name, nickname, email, cellphoneNo);
 
         return loginId + "님 회원가입 완료";
